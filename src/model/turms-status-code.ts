dccd4958df4ac116263faffe3c73d2eb60c7d58b
jameschen <Jameson.Chen@morningstar.com>
2019-10-29 19:05:40 +0800

    Polish the descriptions of turms status codes

--- src/model/turms-status-code.ts
+++ src/model/turms-status-code.ts
@@ -39,23 +39,23 @@ export default class TurmsStatusCode {
         [Code.ALREADY_UP_TO_DATE]: "Already up-to-date",
         [Code.RECIPIENTS_OFFLINE]: "The recipients are offline",
 
-        [Code.DISABLE_FUNCTION]: "The function has been disable in servers",
-        [Code.EXPIRY_DATE_BEFORE_NOW]: "The expiry date needs to be after now",
+        [Code.DISABLE_FUNCTION]: "The function has been disabled in servers",
+        [Code.EXPIRY_DATE_BEFORE_NOW]: "Expiration date must be greater than now",
         [Code.EXPIRY_RESOURCE]: "The target resource has expired",
-        [Code.ID_DUPLICATED]: "The ID is duplicated",
+        [Code.ID_DUPLICATED]: "ID must be unique",
         [Code.ILLEGAL_ARGUMENTS]: "Illegal arguments",
-        [Code.ILLEGAL_DATE_FORMAT]: "The date format is illegal",
-        [Code.OWNED_RESOURCE_LIMIT_REACHED]: "The limit of the resource has been reached",
+        [Code.ILLEGAL_DATE_FORMAT]: "Illegal date format",
+        [Code.OWNED_RESOURCE_LIMIT_REACHED]: "The resource limit is reached",
         [Code.REQUESTED_RECORDS_TOO_MANY]: "Too many records are requested",
         [Code.RESOURCES_HAVE_BEEN_HANDLED]: "The resources have been handled",
         [Code.RESOURCES_HAVE_CHANGED]: "The resources have been changed",
         [Code.SESSION_SIMULTANEOUS_CONFLICTS_DECLINE]: "A different device has logged into your account",
         [Code.SESSION_SIMULTANEOUS_CONFLICTS_NOTIFY]: "Someone attempted to log into your account",
         [Code.SESSION_SIMULTANEOUS_CONFLICTS_OFFLINE]: "A different device has logged into your account",
-        [Code.SUCCESSOR_NOT_GROUP_MEMBER]: "The successor is not group member",
+        [Code.SUCCESSOR_NOT_GROUP_MEMBER]: "The successor is not the group member",
         [Code.TARGET_USERS_UNAUTHORIZED]: "The target users are unauthorized",
         [Code.UNAUTHORIZED]: "Unauthorized",
-        [Code.LOGGED_DEVICES_CANNOT_OFFLINE]: "Cannot set logged devices offline"
+        [Code.LOGGED_DEVICES_CANNOT_OFFLINE]: "Cannot set logged in devices offline"
     };
 
     static isSuccessCode(code: number | string | Code): boolean {
